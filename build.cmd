df26b3243a3859be2186e78eff6bd5116075e5c1
Paal Øye-Strømme <paal.o.eye@gmail.com>
2026-02-28 00:01:40 +0100

    feat: parallel idl generation
    - parallel IDL generation using start /b with wait loop
    - batch processing for includes generation based on NUMBER_OF_PROCESSORS
    - subroutines for batch completion synchronisation
    - syntax quality of life improvements
    
    Signed-Off-By: Paal Øye-Strømme <paal.o.eye@gmail.com>

--- build.cmd
+++ build.cmd
@@ -1,43 +1,133 @@
 @echo off
+setlocal enabledelayedexpansion
 
 if not exist packages (
   mkdir packages
   pushd packages
   echo Downloading nuget packages ...
-  curl.exe -fLo Microsoft.WindowsAppSDK.nupkg                        https://www.nuget.org/api/v2/package/Microsoft.WindowsAppSDK/1.8.260209005                        || exit /b 1
-  curl.exe -fLo Microsoft.WindowsAppSDK.Base.nupkg                   https://www.nuget.org/api/v2/package/Microsoft.WindowsAppSDK.Base/1.8.251216001                   || exit /b 1
-  curl.exe -fLo Microsoft.WindowsAppSDK.Foundation.nupkg             https://www.nuget.org/api/v2/package/Microsoft.WindowsAppSDK.Foundation/1.8.260203002             || exit /b 1
-  curl.exe -fLo Microsoft.WindowsAppSDK.InteractiveExperiences.nupkg https://www.nuget.org/api/v2/package/Microsoft.WindowsAppSDK.InteractiveExperiences/1.8.260125001 || exit /b 1
-  curl.exe -fLo Microsoft.WindowsAppSDK.WinUI.nupkg                  https://www.nuget.org/api/v2/package/Microsoft.WindowsAppSDK.WinUI/1.8.260204000                  || exit /b 1
-  curl.exe -fLo Microsoft.WindowsAppSDK.DWrite.nupkg                 https://www.nuget.org/api/v2/package/Microsoft.WindowsAppSDK.DWrite/1.8.25122902                  || exit /b 1
-  curl.exe -fLo Microsoft.WindowsAppSDK.Widgets.nupkg                https://www.nuget.org/api/v2/package/Microsoft.WindowsAppSDK.Widgets/1.8.251231004                || exit /b 1
-  curl.exe -fLo Microsoft.WindowsAppSDK.Runtime.nupkg                https://www.nuget.org/api/v2/package/Microsoft.WindowsAppSDK.Runtime/1.8.260209005                || exit /b 1
-  rem curl.exe -fLo Microsoft.Windows.SDK.BuildTools.nupkg               https://www.nuget.org/api/v2/package/Microsoft.Windows.SDK.BuildTools/10.0.26100.7705             || exit /b 1
-  rem curl.exe -fLo Microsoft.Windows.CppWinRT.nupkg                     https://www.nuget.org/api/v2/package/Microsoft.Windows.CppWinRT/2.0.250303.1                      || exit /b 1
-  curl.exe -fLo Microsoft.Web.WebView2.nupkg                         https://www.nuget.org/api/v2/package/Microsoft.Web.WebView2/1.0.3800.47                           || exit /b 1
+
+  curl.exe -fLo Microsoft.WindowsAppSDK.nupkg                                  ^
+    https://www.nuget.org/api/v2/package/Microsoft.WindowsAppSDK/1.8.260209005 ^
+    || exit /b 1
+
+  curl.exe -fLo Microsoft.WindowsAppSDK.Base.nupkg                                  ^
+    https://www.nuget.org/api/v2/package/Microsoft.WindowsAppSDK.Base/1.8.251216001 ^
+    || exit /b 1
+
+  curl.exe -fLo Microsoft.WindowsAppSDK.Foundation.nupkg                                  ^
+    https://www.nuget.org/api/v2/package/Microsoft.WindowsAppSDK.Foundation/1.8.260203002 ^
+    || exit /b 1
+
+  curl.exe -fLo Microsoft.WindowsAppSDK.InteractiveExperiences.nupkg                                  ^
+    https://www.nuget.org/api/v2/package/Microsoft.WindowsAppSDK.InteractiveExperiences/1.8.260125001 ^
+    || exit /b 1
+
+  curl.exe -fLo Microsoft.WindowsAppSDK.WinUI.nupkg                                  ^
+    https://www.nuget.org/api/v2/package/Microsoft.WindowsAppSDK.WinUI/1.8.260204000 ^
+    || exit /b 1
+
+  curl.exe -fLo Microsoft.WindowsAppSDK.DWrite.nupkg                                 ^
+    https://www.nuget.org/api/v2/package/Microsoft.WindowsAppSDK.DWrite/1.8.25122902 ^
+    || exit /b 1
+
+  curl.exe -fLo Microsoft.WindowsAppSDK.Widgets.nupkg                                  ^
+    https://www.nuget.org/api/v2/package/Microsoft.WindowsAppSDK.Widgets/1.8.251231004 ^
+    || exit /b 1
+
+  curl.exe -fLo Microsoft.WindowsAppSDK.Runtime.nupkg                                  ^
+    https://www.nuget.org/api/v2/package/Microsoft.WindowsAppSDK.Runtime/1.8.260209005 ^
+    || exit /b 1
+
+  rem curl.exe -fLo Microsoft.Windows.SDK.BuildTools.nupkg                                  ^
+  rem https://www.nuget.org/api/v2/package/Microsoft.Windows.SDK.BuildTools/10.0.26100.7705 ^
+  rem || exit /b 1
+
+  rem curl.exe -fLo Microsoft.Windows.CppWinRT.nupkg                               ^
+  rem https://www.nuget.org/api/v2/package/Microsoft.Windows.CppWinRT/2.0.250303.1 ^
+  rem || exit /b 1
+
+  curl.exe -fLo Microsoft.Web.WebView2.nupkg                                ^
+    https://www.nuget.org/api/v2/package/Microsoft.Web.WebView2/1.0.3800.47 ^
+    || exit /b 1
+
   echo Unpacking nuget packages ...
   for %%f in (*.nupkg) do tar.exe -xf %%f || exit /b 1
   popd
 )
 
 if not exist idl (
-  echo Generating idl files ...
-  winmdidl.exe /nologo /outdir:idl /metadata_dir:packages\metadata packages\metadata\Microsoft.UI.Xaml.winmd                 2>nul
-  winmdidl.exe /nologo /outdir:idl /metadata_dir:packages\metadata packages\metadata\Microsoft.UI.Text.winmd                 2>nul
-  winmdidl.exe /nologo /outdir:idl /metadata_dir:packages\metadata packages\metadata\10.0.18362.0\Microsoft.UI.winmd         2>nul
-  winmdidl.exe /nologo /outdir:idl /metadata_dir:packages\metadata packages\metadata\10.0.18362.0\Microsoft.Foundation.winmd 2>nul
-  winmdidl.exe /nologo /outdir:idl /metadata_dir:packages\metadata packages\metadata\10.0.18362.0\Microsoft.Graphics.winmd   2>nul
-  winmdidl.exe /nologo /outdir:idl /metadata_dir:packages\metadata packages\lib\Microsoft.Web.WebView2.Core.winmd            2>nul
+  echo Generating IDL files ...
+  start /b winmdidl.exe /nologo /outdir:idl /metadata_dir:packages\metadata packages\metadata\Microsoft.UI.Xaml.winmd ^
+    2>nul
+  start /b winmdidl.exe /nologo /outdir:idl /metadata_dir:packages\metadata packages\metadata\Microsoft.UI.Text.winmd ^
+    2>nul
+  start /b winmdidl.exe /nologo /outdir:idl /metadata_dir:packages\metadata packages\metadata\10.0.18362.0\Microsoft.UI.winmd ^
+    2>nul
+  start /b winmdidl.exe /nologo /outdir:idl /metadata_dir:packages\metadata packages\metadata\10.0.18362.0\Microsoft.Foundation.winmd ^
+    2>nul
+  start /b winmdidl.exe /nologo /outdir:idl /metadata_dir:packages\metadata packages\metadata\10.0.18362.0\Microsoft.Graphics.winmd ^
+    2>nul
+  start /b winmdidl.exe /nologo /outdir:idl /metadata_dir:packages\metadata packages\lib\Microsoft.Web.WebView2.Core.winmd ^
+    2>nul
+
+  :wait_idl
+    tasklist | find /i "winmdidl.exe" >nul 2>&1
+    if %errorlevel% equ 0 (
+      timeout /t 1 /nobreak >nul
+      goto wait_idl
+    )
+
+  echo DONE
 )
 
 if not exist includes (
   echo Generating includes, this will take a while ...
   mkdir includes
-  for %%f in (idl\*.idl) do midlrt.exe %%f /nologo /out includes /winrt /ns_prefix /nomd /I idl /I packages\include 2>nul
+
+  set batch_size=%NUMBER_OF_PROCESSORS%
+  echo Using batch size: !batch_size!
+
+  set /a batch_count=0
+  for %%f in (idl\*.idl) do (
+    echo !batch_count! Processing %%f ...
+    start /b midlrt.exe %%f ^
+      /nologo /out includes ^
+      /winrt                ^
+      /ns_prefix            ^
+      /nomd                 ^
+      /I idl                ^
+      /I packages\include 2>nul
+    set /a batch_count+=1
+
+    rem TODO: use modulo operation
+    if !batch_count! equ !batch_size! (
+      echo Waiting for batch to complete...
+      call :wait_for_batch
+      set /a batch_count=0
+    )
+  )
+
+  echo Waiting for final batch ...
+  call :wait_for_batch
 
   rem idk how to generate this, but empty file works fine
   echo. >includes\Microsoft.Windows.ApplicationModel.Resources.h
+
+  echo DONE
 )
+
+goto :after_subroutines
+
+:wait_for_batch
+
+  tasklist | find /i "midlrt.exe" >nul 2>&1
+  if !errorlevel! equ 0 (
+    timeout /t 1 /nobreak >nul
+    goto wait_for_batch
+  )
+  exit /b
+
+:after_subroutines
 
 if not exist output (
   mkdir output
@@ -65,7 +155,21 @@ if not exist output (
   copy /y packages\runtimes-framework\win-x64\native\MRM.dll                                          output\ 1>nul
 )
 
-rem main.manifest is generated by C# code with GenerateAppManifestFromAppx step from packages\build\Microsoft.WindowsAppSDK.SelfContained.targets file
+rem main.manifest is generated by C# code with GenerateAppManifestFromAppx step from
+rem packages\build\Microsoft.WindowsAppSDK.SelfContained.targets file
 rem as input it uses AppxManifest.xml file from packages\tools\MSIX\win10-x64\Microsoft.WindowsAppRuntime.1.8.msix archive
 
-cl.exe /nologo /Z7 /O2 /MT /W3 /Iincludes main.c /link /incremental:no /opt:ref /opt:icf /out:output\main.exe /manifest:embed /manifestinput:main.manifest /defaultlib:ucrt.lib /nodefaultlib:libucrt.lib
+cl.exe  /nologo                      ^
+        /Z7                          ^
+        /O2                          ^
+        /MT                          ^
+        /W3                          ^
+        /Iincludes main.c            ^
+        /link /incremental:no        ^
+        /opt:ref                     ^
+        /opt:icf                     ^
+        /out:output\main.exe         ^
+        /manifest:embed              ^
+        /manifestinput:main.manifest ^
+        /defaultlib:ucrt.lib         ^
+        /nodefaultlib:libucrt.lib
